d83ed665d73f7311f6112ce56100295b837d5b34
Mohammad Jibran <yunisquadri99@gmail.com>
2025-03-10 02:23:05 +0530

    pagination filters and search working

--- inventory/models.py
+++ inventory/models.py
@@ -26,6 +26,7 @@ class VehicleMasterImage(models.Model):
 class CompanyInventory(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="inventory")
     vehicle = models.ForeignKey(VehicleMaster, on_delete=models.CASCADE, related_name="company_inventory")
+    price = models.DecimalField(max_digits=20, decimal_places=2 , default = 500000.00)
     quantity = models.PositiveIntegerField(default=1)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -46,7 +47,7 @@ class CompanyInventoryImage(models.Model):
 
 
 
-
+ 
 class VehicleDocument(models.Model):
     inventory = models.ForeignKey(CompanyInventory, on_delete=models.CASCADE, related_name="documents")
     document_type = models.CharField(max_length=100)  # e.g., Registration, Insurance
--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -21,6 +21,7 @@ AUTH_USER_MODEL = "login.User"
 INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
+    "django_filters",
     "login",
     'inventory',
     "django.contrib.admin",
@@ -36,6 +37,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+
 }
 
 MIDDLEWARE = [
