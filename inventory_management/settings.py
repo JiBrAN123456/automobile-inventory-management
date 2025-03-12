04da1a8febd0886f2b5d5ba9b636139702d66824
Mohammad Jibran <yunisquadri99@gmail.com>
2025-03-12 21:12:56 +0530

    pagination,filters and sorting done

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django_filters",
     "login",
     'inventory',
+    #'corsheaders',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -43,6 +44,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+   # 'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import LogoutView, CustomLoginView , ProfileView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,  # Generates Access & Refresh tokens
+    TokenRefreshView,      # Generates a new Access token using Refresh token
+)
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='custom_login'),
